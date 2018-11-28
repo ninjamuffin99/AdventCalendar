1156a78f583fda925d0c24ef7cae1a597ed8707a
cameron taylor <cameron.taylor.ninja@gmail.com>
2018-11-28 12:16:24 -0800

    moved the gallery stuff to its own substate

---
+++ source/GallerySubstate.hx
@@ -0,0 +1,273 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+import flixel.group.FlxSpriteGroup;
+import flixel.math.FlxAngle;
+import flixel.math.FlxMath;
+import flixel.math.FlxPoint;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+
+/**
+ * ...
+ * @author NInjaMuffin99
+ */
+class GallerySubstate extends FlxSubState 
+{
+	
+	private var picNum:Int;
+	private var picsArray:Array<Dynamic> = [];
+	private var isSpritesheet:Bool = false;
+	private var curAnimPlaying:Int = 0;
+	private var imageText:FlxText;
+	private var bigPreview:FlxSprite;
+	private var bigImage:FlxSpriteGroup;
+	
+	
+	public function new(picNum:Int) 
+	{
+		this.picNum = picNum;
+		
+		picsArray = PlayState.grid;
+		
+		super();
+		
+	}
+	
+	override public function create():Void 
+	{
+		
+		bigImage = new FlxSpriteGroup();
+		bigPreview = new FlxSprite();
+		bigImage.add(bigPreview);
+		
+		imageText = new FlxText(0, FlxG.height - 70, FlxG.width - 6, "Test Words", 18);
+		imageText.alignment = FlxTextAlign.CENTER;
+		imageText.screenCenter(X);
+		bigImage.add(imageText);
+		
+		var text:FlxText = new FlxText(10, 10, 0, "Current Pic - Press ESC to exit", 16);
+		add(text);
+		
+		
+		add(bigImage);
+		bigImage.visible = false;
+		
+		openImage(picNum);
+		
+		super.create();
+	}
+	
+	
+	private function openImage(i:Int):Void
+	{
+		curAnimPlaying = 0;
+		bigImage.visible = true;
+		bigPreview.loadGraphic(picsArray[i][0]);
+		
+		var isAnimated = picsArray[i][2];
+		var horizSize:Int = Std.int(bigPreview.width);
+		var vertSize:Int = Std.int(bigPreview.height);
+		// checks if animated
+		if (isAnimated && !isSpritesheet)
+		{
+			isAnimated = true;
+			horizSize = Std.int(horizSize / picsArray[i][3]);
+			vertSize = Std.int(vertSize / picsArray[i][4]);
+		}
+		
+		bigPreview.loadGraphic(picsArray[i][0], isAnimated, horizSize, vertSize);
+		
+		// loads animation data
+		if (isAnimated && !isSpritesheet)
+		{
+			for (a in 0...picsArray[i][5].length)
+			{
+				bigPreview.animation.add(picsArray[i][5][a][0], picsArray[i][5][a][1], picsArray[i][5][a][2]);
+				bigPreview.animation.play(picsArray[i][5][a][0]);
+			}
+		}
+		
+		if (bigPreview.width < bigPreview.height)
+			bigPreview.setGraphicSize(0, Std.int(FlxG.width * 0.75));
+		else
+			bigPreview.setGraphicSize(Std.int(FlxG.height * 0.75));
+		
+		bigPreview.updateHitbox();
+		bigPreview.screenCenter();
+		
+		imageText.text = picsArray[i][1];
+		
+	}
+	
+	override public function update(elapsed:Float):Void 
+	{
+		#if !mobile
+			keyboardControls();
+		#end
+		
+		dragControls();
+		
+		super.update(elapsed);
+	}
+	
+	
+	private function keyboardControls():Void
+	{
+		#if !mobile
+		if (FlxG.mouse.wheel != 0)
+		{
+			bigPreview.setGraphicSize(Std.int(bigPreview.width + (FlxG.mouse.wheel * 1.5)));
+			bigPreview.updateHitbox();
+			bigPreview.screenCenter();
+		}
+		#end
+		
+		if (FlxG.keys.justPressed.ESCAPE)
+			close();
+		
+		// REPLACE THESE TO BE CLEANER LATER AND WITH MORE KEYS
+		if (FlxG.keys.pressed.S)
+		{
+			bigPreview.offset.y += 10;
+		}
+		if (FlxG.keys.pressed.W)
+		{
+			bigPreview.offset.y -= 10;
+		}
+		
+		if (FlxG.keys.pressed.D)
+		{
+			bigPreview.offset.x += 10;
+		}
+		
+		if (FlxG.keys.pressed.A)
+		{
+			bigPreview.offset.x -= 10;
+		}
+		
+	}
+	
+	private var dragPos:FlxPoint = new FlxPoint();
+	private var picPosOld:FlxPoint = new FlxPoint();
+	
+	private var touchesLength:Float = 0;
+	private var touchesAngle:Float = 0;
+	private var picAngleOld:Float = 0;
+	private var picWidthOld:Float = 0;
+	
+	private function dragControls():Void
+	{	
+		var pressingButton:Bool = false;
+		var zoomPressingButton:Bool = false;
+		var buttonJustPressed:Bool = false;
+		var zoomButtonJustPressed:Bool = false;
+		var buttonPos:FlxPoint = new FlxPoint();
+		
+		// its called touchNew, but really its the length of the line between the two touches
+		// or the length between the center of the image and the mouse on right click
+		var touchNew:Float = 0;
+		var rads:Float = 0;
+		var midScreen:FlxPoint = new FlxPoint();
+		midScreen.set(FlxG.width / 2, FlxG.height / 2);
+				
+		
+		#if !mobile
+			if (FlxG.mouse.pressed)
+			{
+				if (FlxG.mouse.justPressed)
+				{
+					dragPos = FlxG.mouse.getPosition();
+					buttonJustPressed = true;
+				}
+				
+				pressingButton = true;
+				buttonPos = FlxG.mouse.getPosition();
+			}
+			
+			if (FlxG.mouse.pressedRight)
+			{
+				if (FlxG.mouse.justPressedRight)
+				{
+					zoomButtonJustPressed = true;
+				}
+				
+				zoomPressingButton = true;
+				
+				rads = Math.atan2(midScreen.y - FlxG.mouse.y, midScreen.x - FlxG.mouse.x);
+				touchNew = FlxMath.vectorLength(midScreen.x - FlxG.mouse.x, midScreen.y - FlxG.mouse.y);
+			}
+			
+		#else
+			if (FlxG.touches.list.length == 1)
+			{
+				if (FlxG.touches.list[0].justPressed)
+				{
+					dragPos = FlxG.touches.list[0].getPosition();
+					buttonJustPressed = true;
+				}
+				
+				pressingButton = true;
+				buttonPos = FlxG.touches.list[0].getPosition();
+			}
+			if (FlxG.touches.list.length == 2)
+			{
+				
+				if (FlxG.touches.list[1].justPressed)
+				{
+					zoomButtonJustPressed = true;
+				}
+				
+				zoomPressingButton = true;
+				
+				rads = Math.atan2(FlxG.touches.list[0].y - FlxG.touches.list[1].y, FlxG.touches.list[0].x - FlxG.touches.list[1].x);
+				touchNew = FlxMath.vectorLength(FlxG.touches.list[0].x - FlxG.touches.list[1].x, FlxG.touches.list[0].y - FlxG.touches.list[1].y);
+			}
+		#end
+		
+		// drag behaviour
+		if (pressingButton)
+		{
+			if (buttonJustPressed)
+			{
+				picPosOld.x = bigPreview.offset.x;
+				picPosOld.y = bigPreview.offset.y;
+			}
+		
+			
+			var xPos:Float = buttonPos.x - dragPos.x;
+			var yPos:Float = buttonPos.y - dragPos.y;
+			
+			bigPreview.offset.x = picPosOld.x - xPos;
+			bigPreview.offset.y = picPosOld.y - yPos;
+			
+		}
+		
+		// zoom behaviour
+		if (zoomPressingButton)
+		{	
+			if (zoomButtonJustPressed)
+			{
+				touchesLength = touchNew;
+				touchesAngle = FlxAngle.asDegrees(rads);
+				picAngleOld = bigPreview.angle;
+				picWidthOld = bigPreview.width;
+			}
+			
+			
+			var degs = FlxAngle.asDegrees(rads);
+			// bigPreview.angle = (picAngleOld + degs - touchesAngle);
+			
+			FlxG.watch.addQuick("Degs/Angle", degs);
+			
+			bigPreview.setGraphicSize(Std.int(picWidthOld * (touchNew / touchesLength)));
+			bigPreview.updateHitbox();
+			bigPreview.screenCenter();
+			
+		}
+	}
+	
+	
+}
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -24,15 +24,7 @@ import io.newgrounds.objects.events.Result.GetDateTimeResult;
  */
 class PlayState extends FlxState 
 {
-
-	private var bigImage:FlxSpriteGroup;
 	private var _grpThumbnails:FlxTypedGroup<FlxSpriteButton>;
-	private var bigPreview:FlxSprite;
-	private var imageText:FlxText;
-	
-	private var curOpen:Int = 0;
-	private var curAnimPlaying:Int = 0;
-	private var isSpritesheet:Bool = false;
 	
 	private var _emitter:FlxEmitter;
 	private var _emitterBG:FlxEmitter;
@@ -40,7 +32,14 @@ class PlayState extends FlxState
 	
 	override public function create():Void 
 	{	
+		#if !mobile
+			FlxG.mouse.visible = true;
+		#end
+		
 		var ngAPI:NGio = new NGio(APIStuff.APIID, APIStuff.EncKey);
+		
+		// curDate is initialized as local time just incase the newgrounds api gunks up
+		curDate = Date.now();
 		
 		NGio.ngDataLoaded.add(function()
 		{
@@ -66,7 +65,6 @@ class PlayState extends FlxState
 				
 			}).send();
 		});
-		
 		
 		_emitter = new FlxEmitter(-130, -50, 200);
 		_emitter.makeParticles(2, 2, FlxColor.WHITE, 200);
@@ -108,23 +106,6 @@ class PlayState extends FlxState
 		_emitterBG.width = FlxG.width + 150;
 		
 		
-		
-		#if !mobile
-			FlxG.mouse.visible = true;
-		#end
-		
-		bigImage = new FlxSpriteGroup();
-		bigPreview = new FlxSprite();
-		bigImage.add(bigPreview);
-		
-		imageText = new FlxText(0, FlxG.height - 70, FlxG.width - 6, "Test Words", 18);
-		imageText.alignment = FlxTextAlign.CENTER;
-		imageText.screenCenter(X);
-		bigImage.add(imageText);
-		
-		var text:FlxText = new FlxText(10, 10, 0, "Gallery - Press ESC to exit", 16);
-		add(text);
-		
 		_grpThumbnails = new FlxTypedGroup<FlxSpriteButton>();
 		add(_grpThumbnails);
 		
@@ -133,10 +114,7 @@ class PlayState extends FlxState
 			var gridPos:FlxPoint = new FlxPoint(120 * (i % 4) + 10, (120 * Std.int(i / 4)) + 60);
 			
 			var gridBG:FlxSpriteButton = new FlxSpriteButton(gridPos.x, gridPos.y, null, function(){
-				curOpen = i;
-				isSpritesheet = false;
-				
-				openImage(curOpen);
+				openSubState(new GallerySubstate(i));
 			});
 			gridBG.makeGraphic(100, 100);
 			_grpThumbnails.add(gridBG);
@@ -155,259 +133,18 @@ class PlayState extends FlxState
 			add(gridThing);
 		}
 		
-		add(bigImage);
-		bigImage.visible = false;
-		
 		super.create();
-	}
-	
-	private function openImage(i:Int):Void
-	{
-		curAnimPlaying = 0;
-		bigImage.visible = true;
-		bigPreview.loadGraphic(grid[i][0]);
-		
-		var isAnimated = grid[i][2];
-		var horizSize:Int = Std.int(bigPreview.width);
-		var vertSize:Int = Std.int(bigPreview.height);
-		// checks if animated
-		if (isAnimated && !isSpritesheet)
-		{
-			isAnimated = true;
-			horizSize = Std.int(horizSize / grid[i][3]);
-			vertSize = Std.int(vertSize / grid[i][4]);
-		}
-		
-		bigPreview.loadGraphic(grid[i][0], isAnimated, horizSize, vertSize);
-		
-		// loads animation data
-		if (isAnimated && !isSpritesheet)
-		{
-			for (a in 0...grid[i][5].length)
-			{
-				bigPreview.animation.add(grid[i][5][a][0], grid[i][5][a][1], grid[i][5][a][2]);
-				bigPreview.animation.play(grid[i][5][a][0]);
-			}
-		}
-		
-		if (bigPreview.width < bigPreview.height)
-			bigPreview.setGraphicSize(0, Std.int(FlxG.width * 0.75));
-		else
-			bigPreview.setGraphicSize(Std.int(FlxG.height * 0.75));
-		
-		bigPreview.updateHitbox();
-		bigPreview.screenCenter();
-		
-		imageText.text = grid[i][1];
-		
 	}
 	
 	override public function update(elapsed:Float):Void 
 	{
-		#if !mobile
-			keyboardControls();
-		#end
-		
-		dragControls();
-		
 		super.update(elapsed);
-	}
-	
-	private function keyboardControls():Void
-	{
-		#if !mobile
-		if (FlxG.mouse.wheel != 0)
-		{
-			bigPreview.setGraphicSize(Std.int(bigPreview.width + (FlxG.mouse.wheel * 1.5)));
-			bigPreview.updateHitbox();
-			bigPreview.screenCenter();
-		}
-		#end
-		
-		if (FlxG.keys.justPressed.ESCAPE)
-			FlxG.switchState(new MenuState());
-		
-		if (FlxG.keys.justPressed.LEFT)
-		{
-			if (grid[curOpen][2])
-			{
-				curAnimPlaying -= 1;
-				if (curAnimPlaying < 0)
-				{
-					curAnimPlaying = grid[curOpen][5].length;
-					curAnimPlaying -= 1;
-				}
-				bigPreview.animation.play(grid[curOpen][5][curAnimPlaying][0]);
-			}
-		}
-		
-		
-		if (FlxG.keys.justPressed.RIGHT)
-		{
-			if (grid[curOpen][2])
-			{
-				curAnimPlaying += 1;
-				if (curAnimPlaying > grid[curOpen][5].length - 1)
-				{
-					curAnimPlaying = 0;
-				}
-				bigPreview.animation.play(grid[curOpen][5][curAnimPlaying][0]);
-			}
-		}
-		
-		if (FlxG.keys.justPressed.E)
-		{
-			isSpritesheet = !isSpritesheet;
-			
-			openImage(curOpen);
-		}
-		
-		
-		// REPLACE THESE TO BE CLEANER LATER AND WITH MORE KEYS
-		if (FlxG.keys.pressed.S)
-		{
-			bigPreview.offset.y += 10;
-		}
-		if (FlxG.keys.pressed.W)
-		{
-			bigPreview.offset.y -= 10;
-		}
-		
-		if (FlxG.keys.pressed.D)
-		{
-			bigPreview.offset.x += 10;
-		}
-		
-		if (FlxG.keys.pressed.A)
-		{
-			bigPreview.offset.x -= 10;
-		}
-		
-	}
-	
-	private var dragPos:FlxPoint = new FlxPoint();
-	private var picPosOld:FlxPoint = new FlxPoint();
-	
-	private var touchesLength:Float = 0;
-	private var touchesAngle:Float = 0;
-	private var picAngleOld:Float = 0;
-	private var picWidthOld:Float = 0;
-	
-	private function dragControls():Void
-	{	
-		var pressingButton:Bool = false;
-		var zoomPressingButton:Bool = false;
-		var buttonJustPressed:Bool = false;
-		var zoomButtonJustPressed:Bool = false;
-		var buttonPos:FlxPoint = new FlxPoint();
-		
-		// its called touchNew, but really its the length of the line between the two touches
-		// or the length between the center of the image and the mouse on right click
-		var touchNew:Float = 0;
-		var rads:Float = 0;
-		var midScreen:FlxPoint = new FlxPoint();
-		midScreen.set(FlxG.width / 2, FlxG.height / 2);
-				
-		
-		#if !mobile
-			if (FlxG.mouse.pressed)
-			{
-				if (FlxG.mouse.justPressed)
-				{
-					dragPos = FlxG.mouse.getPosition();
-					buttonJustPressed = true;
-				}
-				
-				pressingButton = true;
-				buttonPos = FlxG.mouse.getPosition();
-			}
-			
-			if (FlxG.mouse.pressedRight)
-			{
-				if (FlxG.mouse.justPressedRight)
-				{
-					zoomButtonJustPressed = true;
-				}
-				
-				zoomPressingButton = true;
-				
-				rads = Math.atan2(midScreen.y - FlxG.mouse.y, midScreen.x - FlxG.mouse.x);
-				touchNew = FlxMath.vectorLength(midScreen.x - FlxG.mouse.x, midScreen.y - FlxG.mouse.y);
-			}
-			
-		#else
-			if (FlxG.touches.list.length == 1)
-			{
-				if (FlxG.touches.list[0].justPressed)
-				{
-					dragPos = FlxG.touches.list[0].getPosition();
-					buttonJustPressed = true;
-				}
-				
-				pressingButton = true;
-				buttonPos = FlxG.touches.list[0].getPosition();
-			}
-			if (FlxG.touches.list.length == 2)
-			{
-				
-				if (FlxG.touches.list[1].justPressed)
-				{
-					zoomButtonJustPressed = true;
-				}
-				
-				zoomPressingButton = true;
-				
-				rads = Math.atan2(FlxG.touches.list[0].y - FlxG.touches.list[1].y, FlxG.touches.list[0].x - FlxG.touches.list[1].x);
-				touchNew = FlxMath.vectorLength(FlxG.touches.list[0].x - FlxG.touches.list[1].x, FlxG.touches.list[0].y - FlxG.touches.list[1].y);
-			}
-		#end
-		
-		// drag behaviour
-		if (pressingButton)
-		{
-			if (buttonJustPressed)
-			{
-				picPosOld.x = bigPreview.offset.x;
-				picPosOld.y = bigPreview.offset.y;
-			}
-		
-			
-			var xPos:Float = buttonPos.x - dragPos.x;
-			var yPos:Float = buttonPos.y - dragPos.y;
-			
-			bigPreview.offset.x = picPosOld.x - xPos;
-			bigPreview.offset.y = picPosOld.y - yPos;
-			
-		}
-		
-		// zoom behaviour
-		if (zoomPressingButton)
-		{	
-			if (zoomButtonJustPressed)
-			{
-				touchesLength = touchNew;
-				touchesAngle = FlxAngle.asDegrees(rads);
-				picAngleOld = bigPreview.angle;
-				picWidthOld = bigPreview.width;
-			}
-			
-			
-			var degs = FlxAngle.asDegrees(rads);
-			// bigPreview.angle = (picAngleOld + degs - touchesAngle);
-			
-			FlxG.watch.addQuick("Degs/Angle", degs);
-			
-			bigPreview.setGraphicSize(Std.int(picWidthOld * (touchNew / touchesLength)));
-			bigPreview.updateHitbox();
-			bigPreview.screenCenter();
-			
-		}
 	}
 	
 	// SYNTAX GUIDE
 	// link to image
 	// Info
-	private var grid:Array<Dynamic> = 
+	public static var grid:Array<Dynamic> = 
 	[
 		[
 			"assets/images/scepterD.png",
